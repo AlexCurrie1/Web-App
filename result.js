7cd389161797cdf82dec5554f9ff313c2d7a9876
AlexCurrie1 <abc95@hotmail.co.uk>
2017-04-21 17:14:17 +0100

    geocode
    attempt

--- result.js
+++ result.js
@@ -12,11 +12,42 @@ $( document ).ready(function() {
 
 });
 
+
+var place = search_string;
+
+geocoder.geocode({'placeId': place.place_id}, function(results, status) {
+
+  if (status !== 'OK') {
+    window.alert('Geocoder failed due to: ' + status);
+    return;
+  }
+  map.setZoom(11);
+  map.setCenter(results[0].geometry.location);
+  // Set the position of the marker using the place ID and location.
+  marker.setPlace({
+    placeId: place.place_id,
+    location: results[0].geometry.location
+  });
+  marker.setVisible(true);
+  infowindowContent.children['place-name'].textContent = place.name;
+  infowindowContent.children['place-id'].textContent = place.place_id;
+  infowindowContent.children['place-address'].textContent =
+      results[0].formatted_address;
+  infowindow.open(map, marker);
+});
+
+
+
+
+
 function myMap() {
 var mapProp= {
-    center:new google.maps.LatLng(51.508742,-0.120850),
+    center:new google.maps.Marker;
     zoom:9,
     disableDefaultUI: true,
 };
 var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
 }
+
+
+      }
