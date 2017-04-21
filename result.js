e16e366a4ce721267556dfee9d2dc656c519a2e5
AlexCurrie1 <abc95@hotmail.co.uk>
2017-04-21 15:48:23 +0100

    svaed reuslts
    saved results.js with map code

--- result.js
+++ result.js
@@ -6,5 +6,12 @@ $( document ).ready(function() {
       return results[1] || 0;
   }
   var search_string = $.urlParam('search');
-  
+
+  function myMap() {
+  var mapProp= {
+      center:new google.maps.LatLng(51.508742,-0.120850),
+      zoom:9,
+  };
+  var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+  }
 });
