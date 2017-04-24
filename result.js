1559c29ca58be768f86f6ca9e3eb527100172b91
AlexCurrie1 <abc95@hotmail.co.uk>
2017-04-24 17:20:21 +0100

    brackets AGAIN

--- result.js
+++ result.js
@@ -21,15 +21,15 @@ var mapProp= {
 
 
 function myMap() {
-      //  var map = new google.maps.Map(document.getElementById('map'), {
+      //  var map = new google.maps.Map(document.getElementById('map'),
       //    zoom: 8,
         //  center: {lat: -34.397, lng: 150.644}
           var latlng = new google.maps.LatLng(-34.397,150.644);
         var map = new google.maps.Map(document.getElementById('map'),{
           center: latlng,
           zoom: 8
-        }
-      });
+        });
+      };
 
     var geocoder = new google.maps.Geocoder();
         document.getElementById('submit').addEventListener('click', function() {
