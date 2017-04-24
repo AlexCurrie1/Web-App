de067e426f179c18f975ce7eaf6b1009eeeafbb8
AlexCurrie1 <abc95@hotmail.co.uk>
2017-04-24 17:17:11 +0100

    comma

--- result.js
+++ result.js
@@ -25,7 +25,7 @@ function myMap() {
       //    zoom: 8,
         //  center: {lat: -34.397, lng: 150.644}
           var latlng = new google.maps.LatLng(-34.397,150.644);
-          map = new google.maps.Map(document.getElementById('map'),
+        var map = new google.maps.Map(document.getElementById('map'),
           center: latlng,
           zoom: 8
         });
