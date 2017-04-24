7ef8c30c817f6ca1c2bbd0004c9afe96de950ebf
AlexCurrie1 <abc95@hotmail.co.uk>
2017-04-24 16:10:22 +0100

    geocode added

--- result.js
+++ result.js
@@ -14,12 +14,38 @@ $( document ).ready(function() {
 
 });
 
-function myMap() {
+/*function myMap() {
 var mapProp= {
     center:new google.maps.LatLng(51.508742,-0.120850),
     zoom:9,
   //  disableDefaultUI: true,
 };
-
+*/
 var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
 }
+
+function myMap() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 8,
+          center: {lat: -34.397, lng: 150.644}
+        });
+        var geocoder = new google.maps.Geocoder();
+
+        document.getElementById('submit').addEventListener('click', function() {
+          geocodeAddress(geocoder, map);
+        });
+      }
+
+      function geocodeAddress(geocoder, resultsMap) {
+        var address = document.getElementById('address').value;
+        geocoder.geocode({'address': address}, function(results, status) {
+          if (status === 'OK') {
+            resultsMap.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+              map: resultsMap,
+              position: results[0].geometry.location
+            });
+          } else {
+            alert('Geocode was not successful for the following reason: ' + status);
+          }
+        });
