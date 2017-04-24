9689799734e6cc05bf18f2fe3bf6712f0a632b9e
AlexCurrie1 <abc95@hotmail.co.uk>
2017-04-24 19:03:04 +0100

    Reverse to long and lat

--- result.js
+++ result.js
@@ -14,11 +14,12 @@ $( document ).ready(function() {
 
 
 function myMap() {
+  var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
         var mapProp= {
-          center:new google.maps.Marker,
+          center:new google.maps.LatLng(51.508742,-0.120850),
           zoom: 8,
         };
-        var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
 
   };
 
