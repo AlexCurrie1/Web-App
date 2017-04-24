a2542b93f484e22205896306566bf898384cad00
AlexCurrie1 <abc95@hotmail.co.uk>
2017-04-24 17:00:10 +0100

    googleMpa

--- result.js
+++ result.js
@@ -28,7 +28,7 @@ function initMap() {
 
     var geocoder = new google.maps.Geocoder();
         document.getElementById('submit').addEventListener('click', function() {
-          geocodeAddress(geocoder, map);
+          geocodeAddress(geocoder, googleMap);
           console.log(geocoder);
         });
   };
@@ -39,7 +39,7 @@ function initMap() {
           if (status === 'OK') {
             resultsMap.setCenter(results[0].geometry.location);
             var marker = new google.maps.Marker({
-              map: resultsMap,
+              googleMap: resultsMap,
               position: results[0].geometry.location
             });
           } else {
