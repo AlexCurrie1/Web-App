c6c37633716c6557e0b61d91fe037ff62d93d402
AlexCurrie1 <abc95@hotmail.co.uk>
2017-04-24 20:06:07 +0100

    test

--- result.js
+++ result.js
@@ -14,13 +14,13 @@ $( document ).ready(function() {
 
 
 function myMap() {
-  var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
         var mapProp= {
           center:new google.maps.LatLng(51.508742,-0.120850),
           zoom: 8,
         };
 
-
+  var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
   };
 
       function geocodeAddress(geocoder, resultsMap) {
