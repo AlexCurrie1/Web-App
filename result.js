5a92dc139acb6a315e7b58b4a04278ccd1ee75ce
AlexCurrie1 <abc95@hotmail.co.uk>
2017-04-24 18:23:29 +0100

    test

--- result.js
+++ result.js
@@ -15,7 +15,7 @@ $( document ).ready(function() {
 
 function myMap() {
         var mapProp= {
-          center:new google.maps.LatLng(51.508742,-0.120850),
+          center:new google.maps.Marker),
           zoom: 8,
         };
         var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
