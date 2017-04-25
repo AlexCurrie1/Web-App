fa8b9d0833fc12abca19a8ac4095d59966a564cf
AlexCurrie1 <abc95@hotmail.co.uk>
2017-04-25 12:53:31 +0100

    ???

--- result.js
+++ result.js
@@ -1,3 +1,4 @@
+
 $( document ).ready(function() {
 
 
@@ -12,12 +13,10 @@ $( document ).ready(function() {
 
 
 /*function myMap() {
-
         var mapProp= {
           center:new google.maps.LatLng(51.508742,-0.120850),
           zoom: 8,
         };
-
   var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
   };
 */
