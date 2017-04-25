5103fa3df80f09794ae97432564e780a8b384f48
AlexCurrie1 <abc95@hotmail.co.uk>
2017-04-25 21:00:30 +0100

    Alex home
    Computer file autocomplete WORKING

--- app.js
+++ app.js
@@ -16,31 +16,5 @@ $( document ).ready(function() {
           var searchBox = new google.maps.places.SearchBox(input);
           map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
-  var places = search_bar.getPlaces();
-
-  var bounds = new google.maps.LatLngBounds();
-  var i, place;
-  for (i = 0; place = places[i]; i++) {
-    (function(place) {
-      var marker = new google.maps.Marker({
-
-        position: place.geometry.location
-      });
-      marker.bindTo('map', search_bar, 'map');
-      google.maps.event.addListener(marker, 'map_changed', function() {
-        if (!this.getMap()) {
-          this.unbindAll();
-        }
-      });
-      bounds.extend(place.geometry.location);
-
-
-    }(place));
-
-  }
-  map.fitBounds(bounds);
-  search_bar.set('map', map);
-  map.setZoom(Math.min(map.getZoom(),12));
-
-
+  
   //google.maps.event.addDomListener(window, 'load', init);
